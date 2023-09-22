c37d35f601e71359d047547c8317faa3a91d2768
Samilop Iter <samilops2@gmail.com>
2023-09-22 15:07:15 +0200

    la c'est bon

--- src/main/java/org/polytech/covid/CovidApiApplication.java
+++ src/main/java/org/polytech/covid/CovidApiApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package org.polytech.covid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class CovidApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(CovidApiApplication.class, args);
 	}
 
 }
--- src/test/java/org/polytech/covid/CovidApiApplicationTests.java
+++ src/test/java/org/polytech/covid/CovidApiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package org.polytech.covid;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class CovidApiApplicationTests {
 
 	@Test
 	void contextLoads() {
