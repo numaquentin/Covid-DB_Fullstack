eac0bc8b6d4f6c3c06a5392ac5244ebd6cedaeb0
numaquentin <numaquentin@gmail.com>
2023-10-05 23:46:21 +0200

    Ajout du frontend

--- src/main/java/org/polytech/covid/web/VaccinationCenterController.java
+++ src/main/java/org/polytech/covid/web/VaccinationCenterController.java
@@ -2,7 +2,9 @@ package org.polytech.covid.web;
 
 import org.polytech.covid.service.VaccinationCenterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class VaccinationCenterController {
     
     @Autowired
