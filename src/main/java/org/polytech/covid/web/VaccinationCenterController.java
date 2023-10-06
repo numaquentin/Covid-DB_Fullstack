85dcf2b46d34c7d659a217dcfff60cdfd8277aad
Samilop Iter <samilops2@gmail.com>
2023-10-06 16:42:09 +0200

    merge commit

--- src/main/java/org/polytech/covid/web/VaccinationCenterController.java
+++ src/main/java/org/polytech/covid/web/VaccinationCenterController.java
@@ -4,14 +4,10 @@ import java.util.List;
 import org.polytech.covid.domain.VaccinationCenter;
 import org.polytech.covid.service.VaccinationCenterService;
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-=======
-import org.springframework.stereotype.Controller;
->>>>>>> 5f2bee18bb84f7d5c6fdc7a76689bfd6b87ebd1d
 
 @Controller
 public class VaccinationCenterController {
